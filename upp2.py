d050287575be323174bc697fa2f87bfe214f000e
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-17 11:54:14 +0200

    aergrg

--- upp2.py
+++ upp2.py
@@ -5,7 +5,8 @@ x = np.linspace(-100,100,1000)
 y = np.linspace(-100,100,1000)
 A = np.meshgrid(x,y)
 
-
+print("BEUTIFUL BIG BOOTY BUTT NAKED WOMEN DON'T FALL OF THE SKY YOU KNOW")
+print("BBTBNWDJFOTSYK")
 plt.scatter(A[0], A[1])
 plt.grid()
 plt.show()
