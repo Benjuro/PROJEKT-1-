e393d611d84e98d62ed6961ae8d4fbcac1fd32b6
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-17 11:46:31 +0200

    34

--- upp2.py
+++ upp2.py
@@ -6,6 +6,7 @@ y = np.linspace(-100,100,1000)
 A = np.meshgrid(x,y)
 #Benjamin fucking hora sluta förstör koden RAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 #tetseegeg
+
 plt.scatter(A[0], A[1])
 plt.grid()
 plt.show()
