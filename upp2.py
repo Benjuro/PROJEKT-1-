b5c0645e6bc5b6509aa42b9a2e01bc44b7af9c91
iiDANTEii <101095156+iiDANTEii@users.noreply.github.com>
2024-04-17 11:57:17 +0200

    commit

--- upp2.py
+++ upp2.py
@@ -4,7 +4,7 @@ import numpy as np
 x = np.linspace(-100,100,1000)
 y = np.linspace(-100,100,1000)
 A = np.meshgrid(x,y)
-
+print("NO MORE PARTIES IN LA")
 print("BEUTIFUL BIG BOOTY BUTT NAKED WOMEN DON'T FALL OF THE SKY YOU KNOW")
 print("BBTBNWDJFOTSYK")
 plt.scatter(A[0], A[1])
