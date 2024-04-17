48c116e0c4f783def5615cda1c969351cc1f3636
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-17 11:39:20 +0200

    sezf

--- upp2.py
+++ upp2.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-reeeee
-print("Hello world")
-# test
-#test
+x = np.linspace(-100,100,1000)
+y = np.linspace(-100,100,1000)
+A = np.meshgrid(x,y)
+
+plt.scatter(A[0], A[1])
+plt.show()
