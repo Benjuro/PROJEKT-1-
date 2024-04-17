df2cf08df82247d7d3cf76a1d440421546d4a1df
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-17 11:44:22 +0200

    234

--- upp2.py
+++ upp2.py
@@ -4,6 +4,9 @@ import numpy as np
 x = np.linspace(-100,100,1000)
 y = np.linspace(-100,100,1000)
 A = np.meshgrid(x,y)
-
+#Benjamin fucking hora sluta förstör koden RAAAAAAAAAAAAAAAAAAAAAAAAAAAH
+#tetseegeg
 plt.scatter(A[0], A[1])
+plt.grid()
 plt.show()
+
