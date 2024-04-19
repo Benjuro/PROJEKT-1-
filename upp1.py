edfdf9ad7523fdafb84808113f88d37bbd63cd6c
iiDANTEii <101095156+iiDANTEii@users.noreply.github.com>
2024-04-19 07:16:50 +0200

    commit

--- upp1.py
+++ upp1.py
@@ -1,1 +1,19 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def f(f0, f, b, t):
+    if f0 == f:
+        return (b * f0 * t )/2 * np.sin(f0 * t)
+    else:
+        return ((b * (f0 ** 2))/(f0**2 - f**2)) * (np.cos(f * t) - np.cos(f0 * t))
+
+T = np.linspace(0,10,1000)
+
+plt.title("\u03C9\u2080 = 260 ")
+plt.scatter(T, f(3 * np.pi, 2 * np.pi, 0.1, T))
+plt.show()
+plt.scatter(T, f(3 * np.pi, 3 * np.pi, 0.1, T))
+plt.show()
