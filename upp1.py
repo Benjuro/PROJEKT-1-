0086bc1c6b8f416279e30931946753884efe02e3
iiDANTEii <101095156+iiDANTEii@users.noreply.github.com>
2024-04-21 01:04:52 +0200

    commit

--- upp1.py
+++ upp1.py
@@ -8,12 +8,17 @@ def f(f0, f, b, t):
     if f0 == f:
         return (b * f0 * t )/2 * np.sin(f0 * t)
     else:
-        return ((b * (f0 ** 2))/(f0**2 - f**2)) * (np.cos(f * t) - np.cos(f0 * t))
+        return ((b * f0 * f0)/(f0 * f0 - f * f)) * (np.cos(f * t) - np.cos(f0 * t))
 
 T = np.linspace(0,10,1000)
 
-plt.title("\u03C9\u2080 = 260 ")
-plt.scatter(T, f(3 * np.pi, 2 * np.pi, 0.1, T))
+plt.scatter(T, f(3 * np.pi, 2 * np.pi, 0.1, T), label = "\u03C9\u2080 \u2260 \u03C9 ")
+plt.ylabel("\u03B8 [rad]")
+plt.xlabel("t [s]")
+plt.legend()
 plt.show()
-plt.scatter(T, f(3 * np.pi, 3 * np.pi, 0.1, T))
+plt.scatter(T, f(3 * np.pi, 3 * np.pi, 0.1, T),  label = "\u03C9\u2080 = \u03C9")
+plt.ylabel("\u03B8 [rad]")
+plt.xlabel("t [s]")
+plt.legend()
 plt.show()
--- upp2.py
+++ upp2.py
@@ -1,13 +1,41 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as ode
 
-x = np.linspace(-100,100,1000)
-y = np.linspace(-100,100,1000)
-A = np.meshgrid(x,y)
-print("NO MORE PARTIES IN LA")
-print("BEUTIFUL BIG BOOTY BUTT NAKED WOMEN DON'T FALL OF THE SKY YOU KNOW")
-print("BBTBNWDJFOTSYK")
-plt.scatter(A[0], A[1])
-plt.grid()
+f0 = 3 * np.pi
+f = 2 * np.pi
+b = 0.1
+y = np.array([0,0])
+T = 9.9
+N = 200
+k = T/N
+tn = 0 
+time = []
+position = []
+def F(t, u):
+    return np.array([u[1], - f0 * f0 * u[0] + b * f0 * f0 * np.cos(f * t)])
+
+for i in range(N):
+    w1 = F(tn, y)
+    w2 = F(tn + k/2, y + k/2 * w1)
+    w3 = F(tn + k/2, y + k/2 * w2)
+    w4 = F(tn + k, y + k*w3)
+    y = k/6 * (w1 + 2*w2 + 2*w3 + w4)
+    tn += k
+    time.append(tn)
+    position.append(y[0])
+
+SOL = ode.solve_ivp(F, (0,T), y, method='RK45', rtol=1e-10, atol=1e-10)
+plt.scatter(SOL.t, SOL.y[0])
+plt.scatter(time, position)
 plt.show()
+
+
+
+print("heheh")
+
+
+
+
+
 
