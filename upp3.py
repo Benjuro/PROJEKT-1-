953b8ededa956d76bb2b4a54b021ea8b3dc6e9d8
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-21 23:09:59 +0200

    f

--- upp3.py
+++ upp3.py
@@ -6,7 +6,7 @@ f = f0
 b = 0.1
 y = np.array([0,0])
 T = 40
-N = 800 
+N = 800
 k = T/N
 tn = 0 
 time = []
@@ -24,7 +24,9 @@ for i in range(N):
     time.append(tn)
     position.append(y[0])
 
-plt.scatter(time, position, label = "\u03C9\u2080 \u2260 \u03C9 ")
+
+
+plt.plot(time, position, label = "\u03C9\u2080 = \u03C9")
 plt.ylabel("\u03B8 [rad]")
 plt.xlabel("t [s]")
 plt.grid()
@@ -41,6 +43,7 @@ tn = 0
 for i in range(N):
     w1 = F(tn, y)
     w2 = F(tn + k/2, y + k/2 * w1)
+
     w3 = F(tn + k/2, y + k/2 * w2)
     w4 = F(tn + k, y + k*w3)
     y = y + k/6 * (w1 + 2*w2 + 2*w3 + w4)
@@ -48,7 +51,27 @@ for i in range(N):
     time.append(tn)
     position.append(y[0])
 
-plt.scatter(time, position,  label = "\u03C9\u2080 = \u03C9")
+plt.plot(time, position,  label = "\u03C9\u2080 ≠ \u03C9")
+plt.ylabel("\u03B8 [rad]")
+plt.xlabel("t [s]")
+plt.grid()
+plt.legend()
+plt.show()
+
+time = []
+position = []
+y = np.array([0,0])
+tn = 0 
+N = 10000
+k = T/N
+
+for i in range(N):
+    y = y + k * F(tn, y)
+    tn += k
+    time.append(tn)
+    position.append(y[0]) 
+
+plt.plot(time, position,  label =f"k = {k}")
 
 plt.ylabel("\u03B8 [rad]")
 plt.xlabel("t [s]")
