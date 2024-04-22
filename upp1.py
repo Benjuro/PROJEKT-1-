b820981cdac4650ddf83ded0ccf245c0b32f435d
Benjuro <167298292+Benjuro@users.noreply.github.com>
2024-04-22 16:52:47 +0200

    r

--- upp1.py
+++ upp1.py
@@ -2,8 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 def f(f0, f, b, t):
     if f0 == f:
         return (b * f0 * t )/2 * np.sin(f0 * t)
@@ -12,12 +10,12 @@ def f(f0, f, b, t):
 
 T = np.linspace(0,10,1000)
 
-plt.scatter(T, f(3 * np.pi, 2 * np.pi, 0.1, T), label = "\u03C9\u2080 \u2260 \u03C9 ")
+plt.plot(T, f(3 * np.pi, 2 * np.pi, 0.1, T), label = "\u03C9\u2080 \u2260 \u03C9 ")
 plt.ylabel("\u03B8 [rad]")
 plt.xlabel("t [s]")
 plt.legend()
 plt.show()
-plt.scatter(T, f(3 * np.pi, 3 * np.pi, 0.1, T),  label = "\u03C9\u2080 = \u03C9")
+plt.plot(T, f(3 * np.pi, 3 * np.pi, 0.1, T),  label = "\u03C9\u2080 = \u03C9")
 plt.ylabel("\u03B8 [rad]")
 plt.xlabel("t [s]")
 plt.legend()
